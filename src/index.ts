83b157bb426467612bced879d0e9a3bf2b21c54e
Lucas Marques <lucasmarques0809@gmail.com>
2023-12-31 12:02:28 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-  
+  import express from 'express';
+  import { config } from 'dotenv';
+
+  config();
+
+  const app = express();
+
+  const PORT = process.env.PORT || 8000;
+
+  app.listen(PORT, () => console.log(`listening on port ${PORT}!`));
+
+  app.get('/', (req, res) => {
+    res.send("Hello World!")
+  })
