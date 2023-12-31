103666e96c9a998a73bf97a3152199ba625f8ae6
Lucas Marques <lucasmarques0809@gmail.com>
2023-12-31 14:27:28 -0300

    feat: add MongoClient with mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,28 @@ import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-user
 import { GetUserController } from "./controllers/get-users/get-users";
 import express from "express";
 import { config } from "dotenv";
+import { MongoClient } from "./database/mongo";
 
-config();
+const main = async () => {
+  config();
 
-const app = express();
+  const app = express();
 
-const PORT = process.env.PORT || 8000;
+  const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}!`));
+  await MongoClient.connect();
 
-app.get("/users", async (req, res) => {
-  const mongoGetUsersRepository = new MongoGetUsersRepository();
+  app.get("/users", async (req, res) => {
+    const mongoGetUsersRepository = new MongoGetUsersRepository();
+  
+    const getUserController = new GetUserController(mongoGetUsersRepository);
+  
+    const { body, statusCode } = await getUserController.handle();
+  
+    res.send(body).status(statusCode);
+  });
 
-  const getUserController = new GetUserController(mongoGetUsersRepository);
+  app.listen(PORT, () => console.log(`listening on port ${PORT}!`));
+};
 
-  const { body, statusCode } = await getUserController.handle();
-
-  res.send(body).status(statusCode);
-});
+main();
